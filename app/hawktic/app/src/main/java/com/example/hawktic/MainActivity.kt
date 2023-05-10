7d7812685ddea4ec027a40143481b3bd1e487a7b
SAASMEEM <46927159+SAASMEEM@users.noreply.github.com>
2023-05-10 13:59:09 +0200

    add center button

--- app/hawktic/app/src/main/java/com/example/hawktic/MainActivity.kt
+++ app/hawktic/app/src/main/java/com/example/hawktic/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.hawktic
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
@@ -8,8 +10,11 @@ import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.CustomZoomButtonsController
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,6 +31,24 @@ class MainActivity : ComponentActivity() {
         val marker = Marker(mapView)
         marker.position = GeoPoint(37.7749, -122.4194)
         mapView.overlays.add(marker)
+
+        val context = this
+        // Add a MyLocationNewOverlay to draw the user's location on the map
+        val myLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(context), mapView)
+        myLocationOverlay.enableMyLocation()
+
+        // Create a button to center the map on the user's location
+        val focusButton = findViewById<Button>(R.id.focus_button)
+        focusButton.setOnClickListener {
+            // Get the user's last known location
+            val location = myLocationOverlay.lastFix
+
+            // If the location is not null, center the map on it
+            if (location != null) {
+                mapView.controller.animateTo(GeoPoint(location))
+            }
+        }
+
     }
 
 
